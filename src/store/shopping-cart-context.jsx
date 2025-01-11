8241c7f08fb9c108a40edad103c2530d3197e703
gyubo <yoon9yubo@gmail.com>
2025-01-11 23:32:21 +0900

    0111 useContext All

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,9 +1,9 @@
 import { createContext } from 'react';
 
 export const CartContext = createContext({
-    items: [],
-    addItemToCart: () => {},
+  items: [],
+  addItemToCart: () => {},
+  updateItemQuantity: () => {},
 });
 // createContext는 react compontent가 들어있는 객체이다.
 // 숫자, 문자열, 객체나 배열 모든것을 기본값으로 전달할 수 있다.
-
